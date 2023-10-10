4d6721f94c765172afa363a97f645174cd89fe00
Pongpun <92890500+Pongpun1@users.noreply.github.com>
2023-10-10 15:05:53 +0700

    Add files via upload

---
+++ Lab5/screens/MealDetailScreen.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { MEALS } from "../data/dummy-data";
+
+const MealDetailScreen = ({ route, navigation }) => {
+  // เขียนโค้ดเพิ่ม เพื่อดึงข้อมูลเกี่ยวกับเมนูอาหารที่ผู้ใช้เลือกเอาไว้
+
+  return (
+    <View style={styles.screen}>
+      {/* <Text>The Meal Detail Screen!</Text> */}
+      <Text>Dish : {route.params.title}</Text>
+      <Text>Steps : {route.params.steps}</Text>
+      <Button
+        title="Go Back to Categories"
+        onPress={() => {
+          // เขียนโค้ดเพิ่ม
+          navigation.navigate("Categories", {
+            prev: "MealDetail",
+          });
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default MealDetailScreen;
