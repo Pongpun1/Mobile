86b40ac088ff700b7a4ab3169a520ab8102223a2
Pongpun <92890500+Pongpun1@users.noreply.github.com>
2023-10-10 14:50:41 +0700

    Add files via upload

---
+++ Lab10/database/firebaseDB.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase/compat";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA__YfVgAJCF_EMxEhXyYJ8qaGaqgpv6uA",
+  authDomain: "mobile-2aaf2.firebaseapp.com",
+  projectId: "mobile-2aaf2",
+  storageBucket: "mobile-2aaf2.appspot.com",
+  messagingSenderId: "14174398848",
+  appId: "1:14174398848:web:7a62c936164951746e3965",
+  measurementId: "G-H00QLVR2HY"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+export default firebase;
